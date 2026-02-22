e93ae6d331366ad774a0f75fca14dc1f013702f9
harshi <harshi@gmail.com>
2026-02-22 15:09:28 +0530

    first commit

---
+++ Tns_core_java/src/org/tns/acc/c2tc/core_java/arraydemo/SingleDimArray.java
@@ -0,0 +1,12 @@
+package org.tns.acc.c2tc.core_java.arraydemo;
+
+public class SingleDimArray {
+	public static void main(String[] args) {
+		int[] singlearray={1,2,3,4,5};
+		for (int i:singlearray)
+		{
+			System.out.print(i + "  ");
+		}
+	}
+
+}
