3fa27683cc5c1c20f6c3cb31aeecd4095fcf7baf
harshi <harshi@gmail.com>
2026-02-22 15:55:20 +0530

    commit

---
+++ Tns_core_java/src/jr_developer/KIA_SpeedLimit.java
@@ -0,0 +1,18 @@
+package jr_developer;
+
+public class KIA_SpeedLimit {
+	public void display_speed()
+	{
+		
+		System.out.println("All KIA have Speed Limit");
+		
+	}
+	 
+	public static void main(String[] args) {
+		  
+		KIA_SpeedLimit ob2=new KIA_SpeedLimit();
+		ob2.display_speed();
+		
+	  }
+
+}
