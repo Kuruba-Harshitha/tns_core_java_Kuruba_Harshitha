d764155980a0a15240a1225f4fbbe7e82b851f5b
harshi <harshi@gmail.com>
2026-02-22 15:47:11 +0530

    commit

---
+++ Tns_core_java/src/org/tns/acc/c2tc/core_java/staticdemo/Static_on_block_demo.java
@@ -0,0 +1,26 @@
+package org.tns.acc.c2tc.core_java.staticdemo;
+
+class Game{
+	static int maxLevel;
+	
+	static {
+		System.out.println("Initializing game first");
+		maxLevel = 50;
+	}
+	
+	void showLevelinfo() {
+		System.out.println("Game has" +" "+ maxLevel+" "  + "level");
+	}
+}
+
+public class Static_on_block_demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Game game = new Game();
+		game.showLevelinfo();
+
+	}
+
+
+}
