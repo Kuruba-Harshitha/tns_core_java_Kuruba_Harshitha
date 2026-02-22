eaf8ed8b4471fad25276a9227bed709593ccc1e3
harshi <harshi@gmail.com>
2026-02-22 15:50:23 +0530

    commit

---
+++ Tns_core_java/src/org/tns/acc/c2tc/core_java/string/StringBuilderDemo.java
@@ -0,0 +1,22 @@
+package org.tns.acc.c2tc.core_java.string;
+
+public class StringBuilderDemo {
+	public static void main(String[] args) {
+		StringBuilder sb=new StringBuilder("Initial Value");
+		
+		sb.append(" -Appended"); //add appende to the end
+		sb.append(" -Again");  //add again to the end
+
+		
+		sb.insert(15, "[insert]");
+		
+		sb.replace(16 ,27,"[replaced]");
+		
+		sb.delete(2, 4);
+		
+		sb.reverse();
+		
+		System.out.println("Final stringbuilder result" +"   " + sb.toString());
+	}
+
+}
