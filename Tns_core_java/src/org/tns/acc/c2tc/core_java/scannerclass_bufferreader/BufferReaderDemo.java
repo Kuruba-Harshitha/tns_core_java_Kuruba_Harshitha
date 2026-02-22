a5538839d9694acb8157c272b7d895bb635d7a81
harshi <harshi@gmail.com>
2026-02-22 15:44:52 +0530

    first commit

---
+++ Tns_core_java/src/org/tns/acc/c2tc/core_java/scannerclass_bufferreader/BufferReaderDemo.java
@@ -0,0 +1,19 @@
+package org.tns.acc.c2tc.core_java.scannerclass_bufferreader;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+public class BufferReaderDemo {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br=new BufferedReader(new FileReader
+				("C:\\Users\\gagan\\OneDrive\\Documents\\1t.txt"));
+		String data="";
+		while((data=br.readLine())!=null) 
+			{
+			System.out.println(data);
+			}
+		br.close();
+	}
+
+}
